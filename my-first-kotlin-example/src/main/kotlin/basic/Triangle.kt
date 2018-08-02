3d4da113b6fab0deb04d69ca4bf118fba468c6a8
purple <shuggie.otis0@gmail.com>
2018-08-03 04:31:00 +0900

    update example

--- my-first-kotlin-example/src/main/kotlin/basic/Triangle.kt
+++ my-first-kotlin-example/src/main/kotlin/basic/Triangle.kt
@@ -2,10 +2,21 @@ package basic
 
 // bottom, heightはプロパティになる
 // プロパティはフィールド、ゲッター、セッター
-class Triangle(val bottom: Int, val height: Int)
+class Triangle(val bottom: Int, val height: Int) {
+    init {
+        require(bottom != 0, {"bottom requires over 1"})
+    }
+    private val s = area(bottom, height)
+    override fun toString(): String = "bottom:${bottom},height:${height},area:${s}"
+    private fun area(b: Int, h: Int): Int = b * h / 2
+}
 
 fun main(args: Array<String>) {
     val tri = Triangle(10, 20)
     println("bottom:" + tri.bottom)
     println("height:" + tri.height)
+    println("tri:" + tri)
+
+    // error!!
+    // val tri2 = Triangle(0, 20)
 }
