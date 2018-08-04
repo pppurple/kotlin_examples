c8a055bf9b315504a5695fc07f7b2b118129f25a
purple <shuggie.otis0@gmail.com>
2018-08-04 20:57:09 +0900

    add basic example

--- my-first-kotlin-example/src/main/kotlin/basic/Triangle.kt
+++ my-first-kotlin-example/src/main/kotlin/basic/Triangle.kt
@@ -7,9 +7,20 @@ class Triangle(val bottom: Int, val height: Int) {
         require(bottom != 0, {"bottom requires over 1"})
     }
     private val s = area(bottom, height)
+
+    // fun plus(t: Triangle): Triangle = Triangle(bottom + t.bottom, height + t.height)
+
+    // 演算子overload
+    operator fun plus(t: Triangle): Triangle = Triangle(bottom + t.bottom, height + t.height)
+    operator fun plus(n: Int): Triangle = Triangle(bottom + n, height + n)
+
+    // override
     override fun toString(): String = "bottom:${bottom},height:${height},area:${s}"
     private fun area(b: Int, h: Int): Int = b * h / 2
 }
+
+// 拡張関数
+operator fun Int.plus(t: Triangle): Triangle = t + this
 
 fun main(args: Array<String>) {
     val tri = Triangle(10, 20)
@@ -19,4 +30,18 @@ fun main(args: Array<String>) {
 
     // error!!
     // val tri2 = Triangle(0, 20)
+
+    // plus
+    val tri2 = Triangle(20, 40)
+    // println("plus:" + tri.plus(tri2))
+
+    // plus operator
+    print("+:")
+    println(tri + tri2)
+    print("+:")
+    println(tri + 100)
+
+    // plus 拡張関数
+    print("+(extension):")
+    println(200 + tri)
 }
