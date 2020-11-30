fa6be4d0d60dba11f7190b6add78ff3574d5c9cd
pppurple <shuggie.otis0@gmail.com>
2020-12-01 04:15:41 +0900

    add cancellation examples

---
+++ kotlin-coroutine-examples/src/main/kotlin/com/example/coroutine/cancellationandtimeout/ClosingResourcesWithFinally.kt
@@ -0,0 +1,24 @@
+package com.example.coroutine.cancellationandtimeout
+
+import kotlinx.coroutines.cancelAndJoin
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import java.time.Instant
+
+fun main() = runBlocking {
+    val job = launch {
+        try {
+            repeat(1_000) { i ->
+                println("job: I'm sleeping $i ... [${Instant.now()}] [${Thread.currentThread().name}]")
+                delay(1_000L)
+            }
+        } finally {
+            println("job: I'm running finally. [${Instant.now()}] [${Thread.currentThread().name}]")
+        }
+    }
+    delay(2_300L)
+    println("main: I'm tired of waiting! [${Thread.currentThread().name}]")
+    job.cancelAndJoin()
+    println("main: Now I can quit [${Thread.currentThread().name}]")
+}
---
+++ kotlin-coroutine-examples/src/main/kotlin/com/example/coroutine/cancellationandtimeout/RunNonCancellableBlock.kt
@@ -0,0 +1,25 @@
+package com.example.coroutine.cancellationandtimeout
+
+import kotlinx.coroutines.*
+import java.time.Instant
+
+fun main() = runBlocking {
+    val job = launch {
+        try {
+            repeat(1_000) { i ->
+                println("job: I'm sleeping $i ... [${Instant.now()}] [${Thread.currentThread().name}]")
+                delay(1_000L)
+            }
+        } finally {
+            withContext(NonCancellable) {
+                println("job: I'm running finally. [${Instant.now()}] [${Thread.currentThread().name}]")
+                delay(2_000L)
+                println("job: And I've just delayed for 1 sec because I'm non-cancelable. [${Instant.now()}] [${Thread.currentThread().name}]")
+            }
+        }
+    }
+    delay(2_300L)
+    println("main: I'm tired of waiting! [${Thread.currentThread().name}]")
+    job.cancelAndJoin()
+    println("main: Now I can quit [${Thread.currentThread().name}]")
+}
