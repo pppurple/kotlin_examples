e06f674241fac2c6e54d2c49f87727ce9e8b5e5f
pppurple <shuggie.otis0@gmail.com>
2021-03-18 04:27:40 +0900

    add coroutine cancellation example

---
+++ kotlin-coroutine-examples/src/main/kotlin/com/example/coroutine/cancellationandtimeout/CancellingCoroutineExecutionUsingCancelAndJoin.kt
@@ -0,0 +1,20 @@
+package com.example.coroutine.cancellationandtimeout
+
+import kotlinx.coroutines.cancelAndJoin
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import java.time.Instant
+
+fun main() = runBlocking {
+    val job = launch {
+        repeat(1_000) { i ->
+            println("job: I'm sleeping $i ... [${Instant.now()}] [${Thread.currentThread().name}]")
+            delay(1_000L)
+        }
+    }
+    delay(2_300L) // delay a bit
+    println("main: I'm tired of waiting! [${Instant.now()}] [${Thread.currentThread().name}]")
+    job.cancelAndJoin() // cancels the job and waits for job's completion
+    println("main: Now I can quit [${Instant.now()}] [${Thread.currentThread().name}]")
+}
