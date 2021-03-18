cf6080fd42e09d88c797454119efa2013b733334
pppurple <shuggie.otis0@gmail.com>
2021-03-19 04:25:37 +0900

    add more comment

--- kotlin-coroutine-examples/src/main/kotlin/com/example/coroutine/cancellationandtimeout/RunNonCancellableBlock.kt
+++ kotlin-coroutine-examples/src/main/kotlin/com/example/coroutine/cancellationandtimeout/RunNonCancellableBlock.kt
@@ -13,13 +13,13 @@ fun main() = runBlocking {
         } finally {
             withContext(NonCancellable) {
                 println("job: I'm running finally. [${Instant.now()}] [${Thread.currentThread().name}]")
-                delay(2_000L)
+                delay(1_000L)
                 println("job: And I've just delayed for 1 sec because I'm non-cancelable. [${Instant.now()}] [${Thread.currentThread().name}]")
             }
         }
     }
-    delay(2_300L)
+    delay(2_300L) // delay a bit
     println("main: I'm tired of waiting! [${Thread.currentThread().name}]")
-    job.cancelAndJoin()
+    job.cancelAndJoin() // cancels the job and waits for its completion
     println("main: Now I can quit [${Thread.currentThread().name}]")
 }
