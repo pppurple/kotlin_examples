b03b1c9f1cb3dc65cdb89fc1c3b3a35eb3cc4a43
pppurple <shuggie.otis0@gmail.com>
2021-04-09 03:59:12 +0900

    more comment

--- kotlin-coroutine-examples/src/main/kotlin/com/example/coroutine/coroutinecontextanddispatchers/DispatchersAndThreads.kt
+++ kotlin-coroutine-examples/src/main/kotlin/com/example/coroutine/coroutinecontextanddispatchers/DispatchersAndThreads.kt
@@ -6,15 +6,19 @@ import java.time.Instant
 @ObsoleteCoroutinesApi
 fun main() = runBlocking<Unit> {
     launch {
+        // context of the parent, main runBlocking coroutine
         println("main runBlocking: I'm working in thread [${Thread.currentThread().name}] [${Instant.now()}]")
     }
     launch(Dispatchers.Unconfined) {
+        // not confined -- will work with main thread
         println("Unconfined: I'm working in thread [${Thread.currentThread().name}] [${Instant.now()}]")
     }
     launch(Dispatchers.Default) {
+        // will get dispatched to DefaultDispatcher
         println("Default: I'm working in thread [${Thread.currentThread().name}] [${Instant.now()}]")
     }
     launch(newSingleThreadContext("MyOwnThread")) {
+        // will get its own new thread
         println("newSingleThreadContext: I'm working in thread [${Thread.currentThread().name}] [${Instant.now()}]")
     }
 }
--- kotlin-coroutine-examples/src/main/kotlin/com/example/coroutine/coroutinecontextanddispatchers/UnconfinedVsConfinedDispatcher.kt
+++ kotlin-coroutine-examples/src/main/kotlin/com/example/coroutine/coroutinecontextanddispatchers/UnconfinedVsConfinedDispatcher.kt
@@ -8,11 +8,13 @@ import java.time.Instant
 
 fun main() = runBlocking<Unit> {
     launch(Dispatchers.Unconfined) {
+        // not confined -- will work with main thread
         println("[${Instant.now()}] Unconfined: I'm working in thread [${Thread.currentThread().name}]")
         delay(500)
         println("[${Instant.now()}] Unconfined: After delay in thread [${Thread.currentThread().name}]")
     }
     launch {
+        // context of the parent, main runBlocking coroutine
         println("[${Instant.now()}] main runBlocking: I'm working in thread [${Thread.currentThread().name}]")
         delay(500)
         println("[${Instant.now()}] main runBlocking: After delay in thread [${Thread.currentThread().name}]")
