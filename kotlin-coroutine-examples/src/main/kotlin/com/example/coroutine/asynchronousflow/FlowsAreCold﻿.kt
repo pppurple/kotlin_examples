73dd0a8eaf8449d14cb2291150cfac6c85778e49
pppurple <shuggie.otis0@gmail.com>
2021-12-30 03:33:36 +0900

    add cold flow example

---
+++ kotlin-coroutine-examples/src/main/kotlin/com/example/coroutine/asynchronousflow/FlowsAreCold﻿.kt
@@ -0,0 +1,28 @@
+package com.example.coroutine.asynchronousflow
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.runBlocking
+import java.time.Instant
+
+fun coldFlow(): Flow<Int> = flow {
+    println("Flow started [${Instant.now()}] [${Thread.currentThread().name}]")
+    for (i in 1..3) {
+        delay(100)
+        emit(i)
+    }
+}
+
+fun main() = runBlocking<Unit> {
+    println("Calling coldFlow... [${Instant.now()}] [${Thread.currentThread().name}]")
+    val flow = coldFlow()
+    println("Calling collect... [${Instant.now()}] [${Thread.currentThread().name}]")
+    flow.collect() { value ->
+        println("$value [${Instant.now()}] [${Thread.currentThread().name}]")
+    }
+    println("Calling collect again... [${Instant.now()}] [${Thread.currentThread().name}]")
+    flow.collect() { value ->
+        println("$value [${Instant.now()}] [${Thread.currentThread().name}]")
+    }
+}
