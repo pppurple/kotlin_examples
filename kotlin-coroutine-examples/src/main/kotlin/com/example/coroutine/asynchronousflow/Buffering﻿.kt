68453ba628c4d27ec3c25eea13e9a53678027be3
pppurple <shuggie.otis0@gmail.com>
2022-01-12 15:56:57 +0900

    add Flow examples

---
+++ kotlin-coroutine-examples/src/main/kotlin/com/example/coroutine/asynchronousflow/Buffering﻿.kt
@@ -0,0 +1,27 @@
+package com.example.coroutine.asynchronousflow
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.runBlocking
+import java.time.Instant
+import kotlin.system.measureTimeMillis
+
+fun simple5(): Flow<Int> = flow {
+    for (i in 1..3) {
+        delay(100) // pretend we are asynchronously waiting 100 ms
+        emit(i) // emit next value
+    }
+}
+
+fun main() = runBlocking {
+    val time = measureTimeMillis {
+        simple5().collect() { value ->
+            delay(300) // pretend we are processing it for 300 ms
+            log(value)
+        }
+    }
+    log("Collected in $time ms")
+}
+
+fun log(msg: Int) = println("$msg [${Instant.now()}] [${Thread.currentThread().name}]")
