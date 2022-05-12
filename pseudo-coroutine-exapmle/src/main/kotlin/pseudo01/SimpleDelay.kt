2fdf688558cf6e5dbbdc39c36bcdd078f0aa79f3
pppurple <shuggie.otis0@gmail.com>
2022-05-13 02:21:04 +0900

    add delay examples

--- pseudo-coroutine-exapmle/src/main/kotlin/pseudo01/SimpleDelay.kt
+++ pseudo-coroutine-exapmle/src/main/kotlin/pseudo01/SimpleDelay.kt
@@ -1,14 +1,16 @@
 package pseudo01
 
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import util.log
 
 fun main() = runBlocking {
     delayHelloWorld()
 }
 
 suspend fun delayHelloWorld() {
-    println("Hello")
-    delay(1000L)
-    println("World!")
+    log("Hello")
+    delay(2000L)
+    log("World!")
 }
