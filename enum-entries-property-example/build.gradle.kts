638c009da34e1201161e709172fc9743cebafe07
pppurple <shuggie.otis0@gmail.com>
2023-05-17 02:12:15 +0900

    add build.gradle.kts

---
+++ enum-entries-property-example/build.gradle.kts
@@ -0,0 +1,24 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.8.21"
+}
+
+group = "com.example.pppurple"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "17"
+}
